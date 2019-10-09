b6c39e5756e2035c7d2d70acb21e89229c928212
Nooaah <noah.chtl@gmail.com>
2019-10-09 10:44:16 +0200

    Creation of CRUD categories & CRUD users

--- includes/functions.php
+++ includes/functions.php
@@ -6,28 +6,92 @@ function get_all_posts()
     $sth = $db->query("SELECT * FROM posts");
     return $sth->fetchAll();
 }
-//CRUD
-function create_post()
+//CRUD posts
+function create_post($title, $content)
 {
     global $db;
     $ins = $db->prepare('INSERT INTO posts(title, content) VALUES(?, ?)');
-    $ins->execute(array('titre', 'contenu'));
+    $ins->execute(array($title, $content));
 }
 function retrieve_post($id)
 {
     global $db;
     $get = $db->prepare('SELECT * FROM posts WHERE id = ?');
     $get->execute(array($id));
+    return $get->fetch();
 }
 function update_post($id, $title, $content)
 {
     global $db;
-    $get = $db->prepare('UPDATE posts SET title = ? AND content = ? WHERE id = ?');
-    $get->execute(array($id, $title, $content));
+    $up = $db->prepare('UPDATE posts SET title = ?, content = ? WHERE id = ?');
+    $up->execute(array($title, $content, $id));
 }
 function delete_post($id)
 {
     global $db;
-    $get = $db->prepare('DELETE FROM posts WHERE id = ?');
+    $del = $db->prepare('DELETE FROM posts WHERE id = ?');
+    $del->execute(array($id));
+}
+
+//CRUD categories
+function get_categorie_id_by_name($name)
+{
+    global $db;
+    $ins = $db->prepare('SELECT * FROM categories WHERE name = ?');
+    $ins->execute(array($name));
+    $ins = $ins->fetch();
+    return $ins['id']; 
+}
+function create_categorie($name)
+{
+    global $db;
+    $ins = $db->prepare('INSERT INTO categories(name) VALUES(?)');
+    $ins->execute(array($name));
+}
+function retrieve_categorie($id)
+{
+    global $db;
+    $get = $db->prepare('SELECT * FROM categories WHERE id = ?');
     $get->execute(array($id));
+    return $get->fetch();
+}
+function update_categorie($id, $name)
+{
+    global $db;
+    $up = $db->prepare('UPDATE categories SET name = ? WHERE id = ?');
+    $up->execute(array($name, $id));
+}
+function delete_categorie($id)
+{
+    global $db;
+    $del = $db->prepare('DELETE FROM categories WHERE id = ?');
+    $del->execute(array($id));
+}
+
+
+//CRUD users
+function create_user($pseudo, $mail, $password)
+{
+    global $db;
+    $ins = $db->prepare('INSERT INTO users(pseudo, mail, password) VALUES(?, ?, ?)');
+    $ins->execute(array($pseudo, $mail, $password));
+}
+function retrieve_user($id)
+{
+    global $db;
+    $get = $db->prepare('SELECT * FROM users WHERE id = ?');
+    $get->execute(array($id));
+    return $get->fetch();
+}
+function update_user($id, $pseudo, $mail, $password)
+{
+    global $db;
+    $up = $db->prepare('UPDATE users SET pseudo = ?, mail = ?, password = ? WHERE id = ?');
+    $up->execute(array($pseudo, $mail, $password, $id));
+}
+function delete_user($id)
+{
+    global $db;
+    $del = $db->prepare('DELETE FROM users WHERE id = ?');
+    $del->execute(array($id));
 }
--- public/index.php
+++ public/index.php
@@ -4,27 +4,61 @@ require_once '../includes/config.php';
 
 $posts = get_all_posts();
 
-//create_post();
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Blog</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <!-- <link href="css/style.css" rel="stylesheet"> -->
+    <link rel="icon" type="image/png" href="https://data.whicdn.com/images/48855885/original.png">
 </head>
 <body>
+
     <div class="container">
         <h1 class="text-center">Blog</h1>
         <div class="row">
             <? foreach($posts as $post): ?>
                 <div class="col-md-4">
-                    <h2><?= $post['title'] ?></h2>
-                    <p><?= $post['content'] ?></p>
+                    <h2><?=$post['title']?></h2>
+                    <p><?=$post['content']?></p>
                 </div>
             <? endforeach; ?>
         </div>
     </div>
+
+
+
+
+    <!-- /FIN DU PROJET-->
+
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript"
+        src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/js/mdb.js"></script>
+    <!-- VueJS
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.10/vue.js"></script>-->
+    <!-- Axios -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.0/axios.min.js"></script>
+    <!-- SCRIPTS
+    <script src="js/app.js"></script>
+    <script src="js/ajax.js"></script>
+    <script src="js/script.js"></script>
+-->
 </body>
 </html>
